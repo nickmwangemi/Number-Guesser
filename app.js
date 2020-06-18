b2df1e23d3f9ef07a6230d04577216149ceedc0b
Nick Mwangemi <nickmwangemi@gmail.com>
2020-06-18 08:51:22 +0300

    Implemented random guesses

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ GAME FUNCTION
 // Game values
 let min = 1,
     max = 10,
-    winningNum = 2,
-    guessesLeft = 3
+    winningNum = getRandomNum(min, max)
+guessesLeft = 3
 
 // UI elements
 const game = document.querySelector('#game'),
@@ -25,6 +25,13 @@ const game = document.querySelector('#game'),
 // Assign UI min and max
 minNum.textContent = min
 maxNum.textContent = max
+
+// play again event listener
+game.addEventListener('mousedown', function(e) {
+    if (e.target.className === 'play-again') {
+        window.location.reload()
+    }
+})
 
 // Listen for guess
 guessBtn.addEventListener('click', function() {
@@ -80,7 +87,18 @@ function gameOver(won, msg) {
         // set message
     setMessage(msg)
 
+    // play again
+    guessBtn.value = 'Play Again'
+    guessBtn.className += 'play-again'
+
 }
+
+
+// get winning number
+function getRandomNum(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
 
 // set message
 function setMessage(msg, color) {
